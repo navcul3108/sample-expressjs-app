7099a7e27caeaa8f092a91cfd2f0de05e793ca02
tichtactichtac <ekkojayce3108@gmail.com>
2022-02-12 21:36:50 +0700

    feat: Add compression and helmet lib

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const compression = require("compression");
+const helmet = require("helmet");
 var app = express();
 
+app.use(compression())
+app.use(helmet())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
